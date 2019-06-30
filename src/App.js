6c85cc55f170b051491714228e260d67c8f6d8fb
webguy83 <curtisyacboski@gmail.com>
2019-06-29 18:24:26 -0700

    new tests

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Blah</h1>
+    <div className="App" data-test="my-component-test-app">
+      <h1 data-test="test-counter-h1">This will display the counter</h1>
+      <button data-test="test-counter-button">Click to cunter the counter</button>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -5,7 +5,32 @@ import App from './App';
 
 Enzyme.configure({ adapter: new EnzymeAdapter() })
 
+const setup = (props, state) => {
+  return shallow(<App {...props} />)
+}
+
+const findAttrVal = (wrapper, value) => {
+  return wrapper.find(`[data-test="${value}"]`)
+}
+
 it('renders without crashing', () => {
-  const wrapper = shallow(<App />);
-  expect(wrapper).toBeTruthy()
+  const wrapper = setup();
+  const component = findAttrVal(wrapper, "my-component-test-app");
+  expect(component.length).toBe(1);
 });
+it('displays a counter', () => {
+  const wrapper = setup();
+  const component = findAttrVal(wrapper, "test-counter-h1");
+  expect(component.length).toBe(1);
+})
+it('displays a button to be clicked', () => {
+  const wrapper = setup();
+  const component = findAttrVal(wrapper, "test-counter-button");
+  expect(component.length).toBe(1);
+})
+it('increments the counter on button clicked', () => {
+
+})
+it('start the counter at 0', () => {
+
+})
